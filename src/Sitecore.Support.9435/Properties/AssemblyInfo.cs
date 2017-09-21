bc119e4e4952b40a9043d407a071e697be540304
DK\IEKA <ieka@sitecore.net>
2017-09-21 14:50:39 +0300

    Fix not appliyng styles after importing.

---
+++ src/Sitecore.Support.9435/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.9435")]
+[assembly: AssemblyProduct("Sitecore.Support.9435")]
+[assembly: ComVisible(false)]
